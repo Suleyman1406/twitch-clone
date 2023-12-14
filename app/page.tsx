9ade326055a266f7e526a7ff06898e2ec2973ab7
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-12-14 16:17:47 +0400

    feat: authentication configured with clerkjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <p className="text-red-500 font-bold">
-      <Button>Hello</Button>
+    <p>
+      <UserButton afterSignOutUrl="/" />
     </p>
   );
 }
