f9657d6f1373f6266ab01e7bdc8237719c8f6e11
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-12-14 20:55:50 +0400

    chore: / changed to public route

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,9 +1,5 @@
 import { UserButton } from "@clerk/nextjs";
 
 export default function HomePage() {
-  return (
-    <p>
-      <UserButton afterSignOutUrl="/" />
-    </p>
-  );
+  return <div className="flex flex-col gap-y-4">Home Page</div>;
 }
