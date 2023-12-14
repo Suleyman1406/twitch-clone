d7298564399a5dc763bc6ae22d62833b9efb40d6
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-12-14 13:01:25 +0400

    chore: shadcn button component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <p className="text-red-500 font-bold">Hello Dadawitch</p>;
+  return (
+    <p className="text-red-500 font-bold">
+      <Button>Hello</Button>
+    </p>
+  );
 }
