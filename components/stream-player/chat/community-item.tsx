0783ede8fe1af16f37bcb7f808d4af2e5f27a521
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-12-17 19:19:16 +0400

    feat: chat community(stream participants) section added

---
+++ components/stream-player/chat/community-item.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { onUnblock } from "@/actions/block";
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+import { cn, stringToColor } from "@/lib/utils";
+import { MinusCircle } from "lucide-react";
+import { useTransition } from "react";
+import { toast } from "sonner";
+
+interface ICommunityItemProps {
+  hostName: string;
+  viewerName: string;
+  participantName?: string;
+  participantIdentity: string;
+}
+export const CommunityItem = ({
+  hostName,
+  viewerName,
+  participantIdentity,
+  participantName,
+}: ICommunityItemProps) => {
+  const [isPending, startTransition] = useTransition();
+  const color = stringToColor(participantName || "");
+  const isSelf = participantName === viewerName;
+  const isHost = viewerName === hostName;
+
+  const handleBlock = () => {
+    if (!participantName || !isSelf || !isHost) return;
+
+    startTransition(() => {
+      onUnblock(participantIdentity)
+        .then(() => {
+          toast.success(`Blocked ${participantName}`);
+        })
+        .catch((e) => {
+          toast.error(e.message || "Something went wrong!");
+        });
+    });
+  };
+
+  return (
+    <div
+      className={cn(
+        "group flex items-center justify-between w-full p-2 rounded-md text-sm hover:bg-white/5",
+        isPending && "opacity-50 pointer-events-none"
+      )}
+    >
+      <p style={{ color }}>{participantName}</p>
+      {isHost && isSelf && (
+        <Hint label="Block">
+          <Button
+            variant="ghost"
+            disabled={isPending}
+            onClick={handleBlock}
+            className="h-auto w-auto p-1 opacity-0 group-hover:opacity-100 transition cursor-pointer"
+          >
+            <MinusCircle className="h-4 w-4 text-muted-foreground" />
+          </Button>
+        </Hint>
+      )}
+    </div>
+  );
+};
--- components/stream-player/chat/index.tsx
+++ components/stream-player/chat/index.tsx
@@ -2,16 +2,18 @@
 import { useEffect, useMemo, useState } from "react";
 import { ConnectionState } from "livekit-client";
 import { useMediaQuery } from "usehooks-ts";
+
+import { ChatVariant, useChatSidebar } from "@/store/use-chat-sidebar";
+
 import {
   useChat,
   useConnectionState,
   useRemoteParticipant,
 } from "@livekit/components-react";
-
-import { ChatVariant, useChatSidebar } from "@/store/use-chat-sidebar";
-import { ChatHeader } from "./header";
-import { ChatForm } from "./form";
-import { ChatList } from "./list";
+import { ChatHeader, ChatHeaderSkeleton } from "./header";
+import { ChatForm, ChatFormSkeleton } from "./form";
+import { ChatList, ChatListSkeleton } from "./list";
+import { ChatCommunity } from "./community";
 
 interface IChatProps {
   hostName: string;
@@ -85,9 +87,23 @@ export const Chat = ({
       )}
       {variant === ChatVariant.COMMUNITY && (
         <>
-          <p>Community Mode</p>
+          <ChatCommunity
+            viewerName={viewerName}
+            hostName={hostName}
+            isHidden={isHidden}
+          />
         </>
       )}
+    </div>
+  );
+};
+
+export const ChatSkeleton = () => {
+  return (
+    <div className="flex flex-col border-l border-b pt-0 h-[calc(100vh-80px)] border-2">
+      <ChatHeaderSkeleton />
+      <ChatListSkeleton />
+      <ChatFormSkeleton />
     </div>
   );
 };
